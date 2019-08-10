3fea0ccc455ce152d90675ac12f1c5779d1b974a
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-08-10 06:58:46 +0530

    Add doc headers

--- src/tendril/conventions/motifs/ingbase.py
+++ src/tendril/conventions/motifs/ingbase.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-This file is part of tendril
-See the COPYING, README, and INSTALL files for more information
+Prototype for Instrumentation Amplifier Gain Motifs (:mod:`tendril.conventions.motifs.ingbase`)
+-----------------------------------------------------------------------------------------------
 """
 
 
